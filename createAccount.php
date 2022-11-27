657a0e5ad0328300c73f2af9bd6580cff3545215
lucas wurtz <lucaswurtz0@gmail.com>
2022-11-27 13:54:47 +0100

    mise en place de la création de compte

--- createAccount.php
+++ createAccount.php
@@ -1,21 +1,78 @@
 <?php
+$errors = '';
+$errorName = '';
+$errorLastname = '';
+$errorEmail = '';
+$errorPassword = '';
+$errorPasswordConfirm = '';
 
-include './functionsSQL.php';
+$host = 'localhost';
+$dbname = 'blog';
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die('Error : (' . $conn->connect_errno . ') ' . $conn->connext_error);
+}
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $nom = $_POST['lastName'];
-    $prenom = $_POST['name'];
-    $mail = $_POST['mail'];
-    $mdp = $_POST['password'];
+    $nom = $_POST['lastName'] ?? '';
+    $prenom = $_POST['name'] ?? '';
+    $mail = $_POST['mail'] ?? '';
+    $mdp = $_POST['password'] ?? '';
+    $mdpConfirm = $_POST['passwordConfirm'] ?? '';
+    date_default_timezone_set('UTC');
     $addDate = date('d.m.y');
     $connexion = 0;
 
     $stmt = $conn->prepare("INSERT INTO users (nom, prenom, mail, mdp, addDate, connexion) VALUES (?, ?, ?, ?, ?, ?)");
 
     $stmt->bind_param('ssssss', $nom, $prenom, $mail, $mdp, $addDate, $connexion);
-    $stmt->execute();
+
+    if (!$nom && !$prenom && !$mail && !$mdp && !$mdpConfirm) {
+        $errors = 'TOUS LES CHAMPS SON VIDE !';
+    } else {
+        if (!$prenom) {
+            $errorName = 'AJOUTER UN PRENOM';
+        } else if (strlen($prenom) < 2) {
+            $errorName = 'VOTRE PRENOM DOIT COMPTENIR AU MINIMUM DEUX CARACTERE';
+        } else if (strlen($prenom) > 128) {
+            $errorName = 'VOTRE PRENOM DOIT COMPTENIR MAXIMUM 128 CARACTERE';
+        }
+
+        if (!$nom) {
+            $errorLastname = 'AJOUTER UN NOM';
+        } else if (strlen($nom) < 2) {
+            $errorLastname = 'VOTRE NOM DOIT COMPTENIR AU MINIMUM DEUX CARACTERE';
+        } else if (strlen($nom) > 128) {
+            $errorLastname = 'VOTRE NOM DOIT COMPTENIR MAXIMUM 128 CARACTERE';
+        }
+
+        if (!$mail) {
+            $errorEmail = 'AJOUTER UN EMAIL';
+        } else if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            $errorEmail = '';
+        } else {
+            $errorEmail = "L'ADRESSE EMAIL EST INVALIDE";
+        }
+
+        if (!$mdp) {
+            $errorPassword = "AJOUTER UN MOT DE PASS";
+        } else if ($mdp !== $mdpConfirm) {
+            $errorPasswordConfirm = "LE MOT DE PASS N'EST PAS LE MEME QUE LE MOT DE PASS DE CONFIRMATION";
+        } else {
+            $stmt->execute();
+            header('Location: /blog_bdd/index.php');
+        }
+
+        if (!$mdpConfirm) {
+            $errorPasswordConfirm = "LE MOT DE PASS DE CONFIRMATION EST MANQUANT";
+        }
+    }
 }
 
 ?>
@@ -40,26 +97,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="form-control">
                 <label for="name">Prénom :</label>
                 <input type="text" name="name" placeholder="Kévin" id="name">
+                <p style="color: red; text-align: center;"><?= $errorName ?></p>
             </div>
 
             <div class="form-control">
                 <label for="lastName">Nom :</label>
                 <input type="text" name="lastName" placeholder="Dujardin" id="lastName">
+                <p style="color: red; text-align: center;"><?= $errorLastname ?></p>
             </div>
 
             <div class="form-control">
                 <label for="mail">Adresse mail :</label>
                 <input type="text" name="mail" placeholder="exemple@exemple.com" id="mail">
+                <p style="color: red; text-align: center;"><?= $errorEmail ?></p>
             </div>
 
             <div class="form-control">
                 <label for="password">Mot de pass :</label>
                 <input type="password" name="password" placeholder="Mot de passe" id="password">
+                <p style="color: red; text-align: center; max-width: 70vw;"><?= $errorPassword ?></p>
             </div>
 
+            <div class="form-control">
+                <label for="passwordConfirm">Confirmation de mot de pass :</label>
+                <input type="password" name="passwordConfirm" placeholder="Confirmée votre mot de passe" id="confirmPass">
+                <p style="color: red; text-align: center; max-width: 70vw;"><?= $errorPasswordConfirm ?></p>
+            </div>
 
-            <label for="passwordConfirm">Confirmation de mot de pass :</label>
-            <input type="password" name="passwordConfirm" placeholder="Confirmée votre mot de passe" id="confirmPass">
+            <p style="color: red; text-align: center;"><?= $errors ?></p>
 
             <input type="submit" class="btn btn-submit">
         </form>
--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,9 @@
     <h1><a href="./index.php">Inifni Blog</a></h1>
     <div class="underline"></div>
     <div class="log">
-        <?php if ($_SERVER['REQUEST_URI'] !== '/blog_bdd/login.php') { ?>
+        <?php if ($_SERVER['REQUEST_URI'] === '/blog_bdd/index.php' || '/blog_bdd/add-article.php') : ?>
             <a href="./login.php">Login <i class="fa-solid fa-user"></i></a>
-        <?php } else if ($_SERVER['REQUEST_URI'] === '/blog_bdd/login.php') {
-        }  ?>
+        <?php endif; ?>
     </div>
     <div id="menuBurger">
         <span id="spanUn"></span>
@@ -14,9 +13,15 @@
     </div>
     <nav>
         <ul>
-            <li class="<?php $_SERVER['REQUEST_URI'] === '/blog_bdd/index.php' ? 'active' : '' ?>"><a href="./index.php">Accueil</a></li>
-            <li class="<?php $_SERVER['REQUEST_URI'] === '/blog_bdd/add-article.php' ? 'active' : '' ?>"><a href="./add-article.php">Ajouter un article</a></li>
-            <li class="<?php $_SERVER['REQUEST_URI'] === '/blog_bdd/profil.php' ? 'active' : '' ?>"><a href="./profil.php">Mon profil</a></li>
+            <li>
+                <a class="<?= $_SERVER['REQUEST_URI'] === '/blog_bdd/index.php' ? 'active' : '' ?>" href="./index.php">Accueil</a>
+            </li>
+            <li>
+                <a class="<?= $_SERVER['REQUEST_URI'] === '/blog_bdd/add-article.php' ? 'active' : '' ?>" href="./add-article.php">Ajouter un article</a>
+            </li>
+            <li>
+                <a class="<?= $_SERVER['REQUEST_URI'] === '/blog_bdd/profil.php' ? 'active' : '' ?>" href="./profil.php">Mon profil</a>
+            </li>
         </ul>
     </nav>
 </header>
